10c94f7a5baa90af8998f867486ef0ba1fad0e89
Shamik Patro <patroshamik@gmail.com>
2023-12-30 07:41:02 -0600

    Added Test

---
+++ Projects/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from manim import *
+
+
+class Test(Scene):
+    def construct(self):
+        basel = MathTex(r"\sum_{n=1}^\infty \frac{1}{n^2} = \frac{\pi^2}{6}")
+        self.play(Write(basel))
+        self.wait(3)
