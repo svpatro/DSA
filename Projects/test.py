bc3bef432a834ca06a395ca23266001a08c1bf52
Shamik Patro <patroshamik@gmail.com>
2023-12-31 11:08:10 -0600

    Testing Speed

--- Projects/test.py
+++ Projects/test.py
@@ -7,4 +7,4 @@ class Test(Scene):
     def construct(self):
         basel = MathTex(r"\sum_{n=1}^\infty \frac{1}{n^2} = \frac{\pi^2}{6}")
         self.play(Write(basel))
-        self.wait(4)
+        self.wait(5)
